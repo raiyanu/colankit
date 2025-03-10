09432833280d68330f7f1210c5958749b15f6df0
Raiyan Ahmed <ray38250968@gmail.com>
2025-03-10 10:31:48 +0530

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), libInjectCss()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		libInjectCss(),
+		dts({ rollupTypes: true }),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "package/colankit/index.tsx"),
 			name: "colankit",
-			// the proper extensions will be added
 			fileName: "colankit",
 		},
 		rollupOptions: {
-			// make sure to externalize deps that shouldn't be bundled
-			// into your library
 			external: ["react", "react-dom", "react/jsx-runtime"],
 			output: {
-				// Provide global variables to use in the UMD build
-				// for externalized deps
 				globals: {
 					react: "React",
 					"react-dom": "ReactDOM",
